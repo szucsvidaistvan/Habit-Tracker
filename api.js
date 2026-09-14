c6554552f3b27b20e1fe6b386ea7e428a1e93606
szucsvidaistvan <szucsvidaistvan@gmail.com>
2026-09-14 11:26:51 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // Supabase kliens és API réteg (API Layer)
-const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co'; // Cseréld ki a sajátodra
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Cseréld ki a sajátodra
-//test
+const SUPABASE_URL = 'https://hcnfoywtoegokphjmzpn.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_Uzsb3AnDYM5bAwKWI5gwwQ_Hb3fwtNH';
+
 const supabase = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) : null;
 
 const API = {
